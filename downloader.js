4d0b720e9ae0331a66b125b1b4a6d5855d0d32ea
Sercan Arğa <mail@sercanarga.com>
2022-11-02 20:06:15 +0300

    kod optimizasyonu yapıldı - layer.web.tr

--- downloader.js
+++ downloader.js
@@ -2,26 +2,22 @@
 var image = document.querySelector("img[crossorigin=anonymous]");
 var video = document.querySelector("video");
 
-if(Array.isArray(image)) {
-    var image = image[image.length-1];
-} else if(Array.isArray(video)) {
-    var video = video[video.length-1];
+var image = Array.isArray(image) && image[image.length - 1];
+var video = Array.isArray(video) && video[video.length - 1];
+
+let href, download;
+if (image != undefined) {
+  href = image.src, download = "wadownload.png";
+} else if (video != undefined) {
+  href = video.src, download = "wadownload.mp4";
 }
 
-if(image!=undefined) {
-var href = image.src;
-var download = "wadownload.png";
-} else if(video!=undefined) {
-var href = video.src;
-var download = "wadownload.mp4";
-}
-
-if(href!=null) {
-var a = document.createElement("a");
-a.href = href;
-a.download = download;
-a.click();
-document.body.removeChild(a);
+if (href != null) {
+  var a = document.createElement("a");
+  a.href = href;
+  a.download = download;
+  a.click();
+  document.body.removeChild(a);
 } else {
-alert("No video or image found to download");
+  alert("No video or image found to download");
 }
